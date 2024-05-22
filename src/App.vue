83e940293a24d5c1dafbb5eb631315a9c6824c4b
nrizh31 <naufalrizhaubi@student.uir.ac.id>
2024-05-22 11:25:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="welcome-heading">Selamat Datang di Toko Laptop NaufalRizh</h1>
+    <DaftarLaptop />
+  </div>
+</template>
+
+<script>
+import DaftarLaptop from './components/DaftarLaptop.vue';
+
+export default {
+  components: { DaftarLaptop }
+}
+</script>
+
+<style scoped>
+.welcome-heading {
+  background-color: black;
+  color: white;
+  font-family: Cambria, 'Times New Roman', Times, serif;
+  text-align: center;
+  padding: 20px;
+}
+</style>
